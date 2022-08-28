401cd382a88314044ad0d6280d2dd4cddd0f7f8a
hakantaylan <hakantaylan@users.noreply.github.com>
2022-08-28 15:32:37 +0300

    aaa

--- src/test/java/com/arhohuttunen/CalculatorTest.java
+++ src/test/java/com/arhohuttunen/CalculatorTest.java
@@ -11,4 +11,11 @@ class CalculatorTest {
         int sum = calculator.add(1, 2);
         Assertions.assertEquals(3, sum);
     }
+
+    @Test
+    void substructNumbers() {
+        Calculator calculator = new Calculator();
+        int result = calculator.substruct(2, 1);
+        Assertions.assertEquals(1, result);
+    }
 }
