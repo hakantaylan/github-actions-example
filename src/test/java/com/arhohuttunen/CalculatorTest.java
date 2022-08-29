b0359ca6d0441a0615b8be396ca744b47004750b
hakantaylan <hakantaylan@users.noreply.github.com>
2022-08-29 11:26:48 +0300

    sonerqube plugin added

--- src/test/java/com/arhohuttunen/CalculatorTest.java
+++ src/test/java/com/arhohuttunen/CalculatorTest.java
@@ -12,10 +12,10 @@ class CalculatorTest {
         Assertions.assertEquals(3, sum);
     }
 
-    @Test
-    void substructNumbers() {
-        Calculator calculator = new Calculator();
-        int result = calculator.substruct(2, 1);
-        Assertions.assertEquals(1, result);
-    }
+//    @Test
+//    void substructNumbers() {
+//        Calculator calculator = new Calculator();
+//        int result = calculator.substruct(2, 1);
+//        Assertions.assertEquals(1, result);
+//    }
 }
